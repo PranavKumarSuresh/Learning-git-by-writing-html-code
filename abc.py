2755565fe2782aed86f6779ea4256b3cc22f2b44
PranavKumarSuresh <39843765+PranavKumarSuresh@users.noreply.github.com>
2018-06-25 21:48:16 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,28 @@
+
+from abc import ABCMeta,abstractmethod
+
+
+class Shape(metaclass = ABCMeta):
+
+	@abstractmethod
+	def area(self):
+		return 0
+
+
+class Square(Shape):
+	side = 4
+	#def area(self):
+		#print("area is:", self.side*4)
+
+
+class Eqtriangle(Shape):
+	side = 20
+	def area(self):
+		print("area is:", self.side*3)
+
+
+eq = Eqtriangle()
+sq = Square()
+
+#sq.area()
+eq.area()
